3b11eb8eebd219026c8ff33247e1b19092fa8a41
Hero9909 <JanGrigat@web.de>
2014-09-09 19:52:23 +0200

    OnEnable

--- HubPlugin/src/de/Hero9909/Hub/Main.java
+++ HubPlugin/src/de/Hero9909/Hub/Main.java
@@ -3,5 +3,11 @@ package de.Hero9909.Hub;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
 
 }
